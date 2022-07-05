9df1d09cda91f2159c2f2cf8ed43e8c5850f30f1
Ext-O.Auezov <oauezov@it-one.ru>
2022-07-05 19:07:45 +0600

    final commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.time.Duration;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
@@ -21,13 +22,17 @@ public class Main {
             driver.findElement(By.id("loginByPwdButton")).click();
             driver.findElement(By.xpath("//button[contains(.,'Ок')]")).click();
             driver.findElement(By.xpath("//button[contains(.,'Новое письмо')]")).click();
+            File file = new File("data/letter.pdf");
+            String absolutePath = file.getAbsolutePath();
+            By fileInput = By.xpath("(//input[@type='file'])[1]");
+            driver.findElement(fileInput).sendKeys(absolutePath);
             driver.findElement(By.xpath("(//input[@value=''])[2]")).sendKeys("Получатель 1");
             wait.until(presenceOfElementLocated(By.className("hand__choice-title")));
             driver.findElement(By.className("hand__choice-title")).click();
             driver.findElement(By.xpath("(//input[@value=''])[2]")).sendKeys("129085, г Москва, Останкинский р-н, проезд Ольминского, д 1");
             Thread.sleep(3000);
             wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//span[.='Письмо будет доставлено в бумажном виде'])[1]")));
-            for(int i = 2; i < 35; i++) {
+            for(int i = 2; i < 10; i++) {
                 wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[contains(.,'Добавить получателя')]")));
                 driver.findElement(By.xpath("//span[contains(.,'Добавить получателя')]")).click();
                 driver.findElement(By.xpath("(//input[@value=''])[2]")).sendKeys("Получатель " + i);
@@ -38,6 +43,7 @@ public class Main {
                 wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//span[.='Письмо будет доставлено в бумажном виде'])["+i+"]")));
             }
             driver.findElement(By.xpath("(//input[@value=''])[2]")).sendKeys("421001, Российская Федерация, Респ Татарстан, г Казань, ул Адоратского, д. 1а, кв. 1");
+            Thread.sleep(3000);
             driver.findElement(By.xpath("//body")).click();
 
         } catch (InterruptedException e) {
